52266890b4df34c70d5f409e0510961caf7b839b
zimarali <zimarali@gmail.com>
2017-07-13 12:53:52 -0400

    cleaned up and added modal to be used.

--- module.js
+++ module.js
@@ -1,22 +1,20 @@
 jQuery(document).ready(function($) {
 
-      $('#myCarousel').carousel({
+  $('#artCarousel').carousel({
               interval: 5000
+    });
+
+  $('#carousel-text').html($('#slide-content-0').html());
+
+
+  $('[id^=carousel-selector-]').click( function(){
+    var id = this.id.substr(this.id.lastIndexOf("-") + 1);
+    var id = parseInt(id);
+  $('#artCarousel').carousel(id);
       });
 
-      $('#carousel-text').html($('#slide-content-0').html());
-
-      //Handles the carousel thumbnails
-     $('[id^=carousel-selector-]').click( function(){
-          var id = this.id.substr(this.id.lastIndexOf("-") + 1);
-          var id = parseInt(id);
-          $('#myCarousel').carousel(id);
-      });
-
-
-      // When the carousel slides, auto update the text
-      $('#myCarousel').on('slid.bs.carousel', function (e) {
-               var id = $('.item.active').data('slide-number');
-              $('#carousel-text').html($('#slide-content-'+id).html());
+  $('#artCarousel').on('slid.bs.carousel', function (e) {
+     var id = $('.item.active').data('slide-number');
+  $('#carousel-text').html($('#slide-content-'+id).html());
       });
 });
